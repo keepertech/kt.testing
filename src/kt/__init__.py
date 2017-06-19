f56bfcb92123588a90e5cea47ee00bd98fc79930
Fred Drake <fred@fdrake.net>
2017-06-19 08:57:20 -0400

    hack around debian packaging failure

--- src/kt/__init__.py
+++ src/kt/__init__.py
@@ -8,6 +8,8 @@ except ImportError:
     __path__ = pkgutil.extend_path(__path__, __name__)
 else:
     pkg_resources.declare_namespace(__name__)
-    del pkg_resources
+    # This breaks when building a debian package; no idea why.
+    # del pkg_resources
 
-del pkgutil
+# This breaks when building a debian package; no idea why.
+# del pkgutil
