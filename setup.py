11ff85272c042d31b740927766e57383530a28c8
Fred Drake <fred.drake@keepertech.com>
2018-07-12 10:25:10 -0400

    expand test coverage to python3.7

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ metadata = dict(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Testing',
     ],
 )
